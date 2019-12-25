b4d15832cfdb22f7a2135760627b191a12cc4156
Jarosław Bieniek <jaroslaw.bieniek255@outlook.com>
2019-12-25 12:04:42 +0100

    homework 3 finished (almost)

---
+++ homework3/ex1/ex1.py
@@ -0,0 +1,83 @@
+import random
+import json
+import numpy as np
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+DIMENSIONS = [
+    3,
+    4,
+    5,
+    7,
+    13
+]
+
+POINTS = 50000
+SIDE_POINTS = 100
+
+RADIUS = 1
+SIDE = 2 * RADIUS
+
+
+def generate_usual_point(dimension):
+    point = [random.uniform(-1.0, 1.0) for _ in range(dimension)]
+    if sum(map(lambda coordinate: coordinate ** 2, point)) <= RADIUS ** 2:
+        return point, [0.0, 1.0, 0.0]
+    else:
+        return point, [0.0, 0.0, 1.0]
+
+
+def corner_points(dimension):
+    return [([float(char) for char in map(lambda x: "-1" if x == "0" else x, list(np.binary_repr(d, width=dimension)))], [1.0, 0.0, 0.0]) for d in range(2 ** dimension)]
+
+
+def side_points(dimension):
+    return [
+        (random.sample([random.choice([-1.0, 1.0]) for _ in range(dimension - 1)] + [random.uniform(-1.0, 1.0)], dimension), [1.0, 1.0, 0.0])
+        for _ in range(SIDE_POINTS)
+    ]
+
+
+def generate_points():
+    dimension_data = {dimension: [generate_usual_point(dimension)
+                                  for _ in range(POINTS)] + side_points(dimension) + corner_points(dimension)
+                      for dimension in DIMENSIONS}
+    return dimension_data
+
+
+def run_pca(dimension_data):
+    pca_result = {2: {}, 3: {}}
+    for dimension in DIMENSIONS:
+        pca2 = PCA(n_components=2)
+        points = dimension_data[dimension]
+        pca_result[2][dimension] = pca2.fit_transform(points).tolist()
+        pca3 = PCA(n_components=3)
+        pca_result[3][dimension] = pca3.fit_transform(points).tolist()
+    return pca_result
+
+
+def run():
+    dimension_data = generate_points()
+    points = {dimension: [point[0] for point in dimension_data[dimension]] for dimension in DIMENSIONS}
+    print(json.dumps(points, indent=2))
+    colors = {dimension: [point[1] for point in dimension_data[dimension]] for dimension in DIMENSIONS}
+    print(json.dumps(colors, indent=2))
+    pca_result = run_pca(points)
+    print(json.dumps(list(zip(*pca_result[2][3])), indent=2))
+    plt.scatter(*zip(*pca_result[2][3]), color=colors[3])
+    plt.show()
+    plt.scatter(*zip(*pca_result[2][4]), color=colors[4])
+    plt.show()
+    plt.scatter(*zip(*pca_result[2][5]), color=colors[5])
+    plt.show()
+    plt.scatter(*zip(*pca_result[2][7]), color=colors[7])
+    plt.show()
+    plt.scatter(*zip(*pca_result[2][13]), color=colors[13])
+    plt.show()
+
+    # print(json.dumps(run_pca(points), indent=2))
+    # print(json.dumps(dimension_data, indent=2))
+
+
+if __name__ == "__main__":
+    run()
---
+++ homework3/ex2/ex2.py
@@ -0,0 +1,100 @@
+import random
+import numpy as np
+from PIL import Image
+import json
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from sklearn.kernel_approximation import RBFSampler
+from sklearn.decomposition import KernelPCA
+
+
+RED = np.array([237, 28, 36])
+GREEN = np.array([34, 177, 76])
+BLUE = np.array([0, 162, 232])
+YELLOW = np.array([255, 242, 0])
+
+
+def prepare_dataset():
+    dataset = {color: [] for color in ["red", "green", "blue", "yellow"]}
+    handle = Image.open("../assets/PCA.png").convert("RGB")
+    image = np.array(handle)
+    shape = image.shape
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            if (image[i][j] == RED).all():
+                dataset["red"].append(((i, j), "red"))
+            elif (image[i][j] == GREEN).all():
+                dataset["green"].append(((i, j), "green"))
+            elif (image[i][j] == BLUE).all():
+                dataset["blue"].append(((i, j), "blue"))
+            elif (image[i][j] == YELLOW).all():
+                dataset["yellow"].append(((i, j), "yellow"))
+    return dataset
+
+
+def run_pca(data):
+    pca2 = PCA(n_components=2)
+    pca_data = pca2.fit_transform(data).tolist()
+    print(pca2.components_)
+    print(pca2.explained_variance_)
+    print(pca2.explained_variance_ratio_)
+    return pca_data, pca2.components_
+
+
+def run_cosine_pca(data, centering):
+    kernel_pca2_cosine = KernelPCA(n_components=2, kernel="cosine")
+    if not centering:
+        data = [[pixel[0] / 5 * ((pixel[0] / 321) ** 3), pixel[1] / 2 * ((pixel[1] / 435) ** 3)] for pixel in data]
+    pca_cosine_data = kernel_pca2_cosine.fit_transform(data)
+    plt.scatter(*zip(*pca_cosine_data))
+    plt.show()
+    pass
+
+
+def run_rbf_pca(data, gamma):
+    kernel_pca2_rbf = KernelPCA(n_components=2, kernel="rbf")
+    kernel_pca2_rbf.gamma = gamma
+    pca_rbf_data = kernel_pca2_rbf.fit_transform(data)
+    plt.scatter(*zip(*pca_rbf_data))
+    plt.show()
+    pass
+
+
+def run_pca_with_kernel(data):
+    run_cosine_pca(data, centering=False)
+    run_cosine_pca(data, centering=True)
+    for gamma in [0.000001, 0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10]:
+        run_rbf_pca(data, gamma)
+
+
+def draw_pca_vectors(pca_components):
+    figure, axes = plt.subplots(1)
+    handle = Image.open("../assets/PCA.png").convert("RGB")
+    image = np.array(handle)
+    axes.imshow(handle)
+    plt.quiver([image.shape[1] / 2], [image.shape[0] / 2], pca_components[:, 0], pca_components[:, 1], scale=10)
+    plt.show()
+
+
+def run():
+    dataset = prepare_dataset()
+    data = [point[0] for point in [item for sublist in dataset.values() for item in sublist]]
+    print(json.dumps(data, indent=2))
+    colors = [point[1] for point in [item for sublist in dataset.values() for item in sublist]]
+    pca_data, pca_components = run_pca(data)
+    # print(json.dumps(pca_data, indent=2))
+    plt.scatter(*zip(*pca_data), color=colors)
+    draw_pca_vectors(pca_components)
+    run_pca_with_kernel(data)
+
+
+if __name__ == "__main__":
+    run()
+
+
+"""
+34 177 76
+0 162 232
+237 28 36
+255 242 0
+"""
