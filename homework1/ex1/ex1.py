aeb217961478062f3a7c6197ced6598807f81cb6
Shino1024 <GH7alkj!@#123>
2019-12-28 16:46:44 +0100

    homework 1 in progress

--- homework1/ex1/ex1.py
+++ homework1/ex1/ex1.py
@@ -4,39 +4,41 @@ from decimal import *
 import math
 import matplotlib.pyplot as plt
 import statistics
+from decimal import Decimal
 
 RADIUS = 1
 SIDE = 2 * RADIUS
-DIMENSIONS = 5
-ITERATIONS = 10
+DIMENSIONS = 15
+ITERATIONS = 20
 
 
-TEST_CASES = {
+TEST_CASES = [
     10,
     20,
     50,
     100,
     200,
     500,
-    1000
-}
+    1000,
+    2000
+]
 
 
 def point_in_hyperball(point):
-    return sum(map(lambda coordinate: coordinate ** 2, point)) < RADIUS ** 2
+    return sum(map(lambda coordinate: coordinate ** 2, point)) <= RADIUS ** 2
 
 
-def generate_points(dimensions):
+def generate_points():
     iteration_data = []
     for i in range(ITERATIONS):
         dimension_data = {}
         for dimension in range(1, DIMENSIONS + 1):
-            test_case_points = []
+            test_case_points = {}
             for test_case in TEST_CASES:
-                points = [
-                    [random.uniform(-1.0, 1.0) for _ in range(dimension)] for _ in range(test_case)
+                coordinates = [
+                    [Decimal(random.uniform(-1.0, 1.0)) for _ in range(dimension)] for _ in range(test_case)
                 ]
-                test_case_points.append(points)
+                test_case_points[test_case] = coordinates
             dimension_data[dimension] = test_case_points
         iteration_data.append(dimension_data)
     return iteration_data
@@ -47,79 +49,93 @@ def select_points_inside_hyperball(data):
     for i in range(ITERATIONS):
         dimension_data = {}
         for dimension in range(1, DIMENSIONS + 1):
-            test_case_hyperball_ratios = []
-            for test_case in data[i][dimension]:
+            test_case_hyperball_ratios = {}
+            for test_case in  TEST_CASES:
                 points_in_hyperball = 0
-                for point in test_case:
+                for point in data[i][dimension][test_case]:
                     if point_in_hyperball(point):
                         points_in_hyperball += 1
-                hyperball_ratio = points_in_hyperball / len(test_case)
-                test_case_hyperball_ratios.append(hyperball_ratio)
+                hyperball_ratio = points_in_hyperball / test_case
+                test_case_hyperball_ratios[test_case] = hyperball_ratio
             dimension_data[dimension] = test_case_hyperball_ratios
         iteration_data.append(dimension_data)
     return iteration_data
 
 
-def iteration_mean(data):
-    mean_data = {dimension: [[] for _ in range(len(TEST_CASES))] for dimension in range(1, DIMENSIONS + 1)}
-    for i in range(ITERATIONS):
-        for dimension in range(1, DIMENSIONS + 1):
-            for j in range(len(TEST_CASES)):
-                mean_data[dimension][j].append(data[i][dimension][j])
-    mean = {dimension: [] for dimension in range(1, DIMENSIONS + 1)}
-    for dimension in range(1, DIMENSIONS + 1):
-        for test_case in range(len(TEST_CASES)):
-            mean[dimension].append(sum(mean_data[dimension][test_case]) / len(mean_data[dimension][test_case]))
-    return mean
-
-
-def test_case_standard_deviation(test_case):
-    standard_deviation = []
-    print(test_case)
-    mean = sum(test_case) / len(test_case)
-    for i in range(len(test_case)):
-        standard_deviation.append((test_case[i] - mean) ** 2)
-    standard_deviation = math.sqrt(sum(standard_deviation) / (len(test_case) - 1))
-    return standard_deviation
-
-
-def iteration_standard_deviation(iteration_data):
-    return list(map(lambda test_case: statistics.stdev(test_case), iteration_data))
-
-
 def calculate_standard_deviation(data):
     dimension_data = {}
     for dimension in range(1, DIMENSIONS + 1):
-        iteration_data = []
-        for i in range(ITERATIONS):
-            iteration_data.append(data[i][dimension])
-        dimension_data[dimension] = iteration_standard_deviation(iteration_data)
-    # dimension_data = {dimension: [] for dimension in range(1, DIMENSIONS + 1)}
-    # iteration_data = []
-    # for i in range(ITERATIONS):
-    #     dimension_data = {}
-    #     for dimension in range(1, DIMENSIONS + 1):
-    #         test_case_data = []
-    #         for test_case in points[i][dimension]:
-    #             test_case_data.append(test_case_standard_deviation(test_case))
-    #         dimension_data[dimension] = test_case_data
-    #     iteration_data.append(dimension_data)
+        test_case_data = {}
+        for test_case in TEST_CASES:
+            iteration_data = []
+            for i in range(ITERATIONS):
+                iteration_data.append(data[i][dimension][test_case])
+            test_case_data[test_case] = statistics.stdev(iteration_data)
+        dimension_data[dimension] = test_case_data
     return dimension_data
 
 
-def plot_data_to_file(data):
-    mean = iteration_mean(data)
-    print(json.dumps(mean, indent=2))
-    standard_deviation = calculate_standard_deviation(data)
-    print(json.dumps(standard_deviation, indent=2))
+def calculate_mean(data):
+    dimension_data = {}
+    for dimension in range(1, DIMENSIONS + 1):
+        test_case_data = {}
+        for test_case in TEST_CASES:
+            iteration_data = []
+            for i in range(ITERATIONS):
+                iteration_data.append(data[i][dimension][test_case])
+            test_case_data[test_case] = statistics.mean(iteration_data)
+        dimension_data[dimension] = test_case_data
+    return dimension_data
+
+
+def plot_data(mean, standard_deviation):
+    for dimension in range(1, DIMENSIONS + 1):
+        dimension_mean = mean[dimension]
+        dimension_standard_deviation = standard_deviation[dimension]
+        plt.xticks(TEST_CASES)
+        plt.xscale("log")
+        plt.scatter(dimension_mean.keys(), dimension_mean.values())
+        plt.errorbar(dimension_mean.keys(), dimension_mean.values(),
+                     xerr=[0 for _ in dimension_standard_deviation.keys()], yerr=dimension_standard_deviation.values())
+        axes = plt.gca()
+        axes.set_ylim(0.0, 1.0)
+        axes.set_xlim(TEST_CASES[0], TEST_CASES[-1] * 2)
+        plt.xlabel("Number of points")
+        plt.ylabel("Hyperball ratio")
+        plt.title("Hyperball ratios after 20 iterations for dimension no " + str(dimension))
+        plt.show()
+    for i, test_case in enumerate(TEST_CASES):
+        means = []
+        standard_deviations = []
+        for dimension in range(1, DIMENSIONS + 1):
+            means.append(mean[dimension][test_case])
+        for dimension in range(1, DIMENSIONS + 1):
+            standard_deviations.append(standard_deviation[dimension][test_case])
+        axes = plt.gca()
+        axes.set_ylim(0.0, 1.0)
+        axes.set_xlim(1, 15)
+        plt.xticks(range(1, DIMENSIONS + 1))
+        plt.scatter(range(1, DIMENSIONS + 1), means, cmap="rainbow")
+        plt.errorbar(range(1, DIMENSIONS + 1), means,
+                     xerr=[0 for _ in range(1, DIMENSIONS + 1)], yerr=standard_deviations)
+        plt.xlabel("Dimensions")
+        plt.ylabel("Hyperball ratio")
+        plt.title("Hyperball ratios after 20 iterations for test case no " + str(i + 1) + " (" + str(test_case) + " points)")
+        plt.show()
 
 
 def run():
     getcontext().prec = 10
-    iteration_data = generate_points(DIMENSIONS)
+    iteration_data = generate_points()
     selected_points = select_points_inside_hyperball(iteration_data)
-    print(json.dumps(selected_points, indent=2))
-    plot_data_to_file(selected_points)
+    # print(json.dumps(selected_points, indent=2))
+    standard_deviation = calculate_standard_deviation(selected_points)
+    mean = calculate_mean(selected_points)
+    plot_data(mean, standard_deviation)
+    print(json.dumps(standard_deviation, indent=2))
+    print(json.dumps(mean, indent=2))
+
+    # plot_data_to_file(selected_points)
     # print(json.dumps(dimension_data, indent=2))
     # percentage_hyperball = map(lambda dimension: sum(dimension) / len(dimension), selected_points.values())
     # print(json.dumps(percentage_hyperball, indent=2))\
--- homework1/ex2/ex2.py
+++ homework1/ex2/ex2.py
@@ -1,30 +1,37 @@
 import random
 import json
 import math
+from decimal import Decimal
+import statistics
 
 RADIUS = 1
 SIDE = 2 * RADIUS
-DIMENSIONS = 3
-ITERATIONS = 4
+DIMENSIONS = 15
+ITERATIONS = 20
 
 
-TEST_CASES = {
+TEST_CASES = [
     10,
-    20
-}
+    20,
+    50,
+    100,
+    200,
+    500,
+    1000
+]
 
 
-def generate_points(dimensions):
+def generate_points():
     iteration_data = []
     for i in range(ITERATIONS):
         dimension_data = {}
         for dimension in range(1, DIMENSIONS + 1):
-            test_case_points = []
+            test_case_points = {}
             for test_case in TEST_CASES:
-                points = [
-                    [random.uniform(-1.0, 1.0) for _ in range(dimension)] for _ in range(test_case)
+                coordinates = [
+                    [Decimal(random.uniform(-1.0, 1.0)) for _ in range(dimension)] for _ in range(test_case)
                 ]
-                test_case_points.append(points)
+                test_case_points[test_case] = coordinates
             dimension_data[dimension] = test_case_points
         iteration_data.append(dimension_data)
     return iteration_data
@@ -34,49 +41,72 @@ def calculate_point_distance(point0, point1):
     return math.sqrt(sum(map(lambda a: (a[0] - a[1]) ** 2, zip(point0, point1))))
 
 
-def test_case_standard_deviation(test_case):
-    standard_deviation = []
-    num_of_distances = len(test_case) * (len(test_case) - 1) / 2
-    mean_distance = test_case_mean_distance(test_case)
-    for i in range(len(test_case)):
-        for j in range(i + 1, len(test_case)):
-            point_distance = calculate_point_distance(test_case[i], test_case[j])
-            standard_deviation.append((point_distance - mean_distance) ** 2)
-    standard_deviation = math.sqrt(sum(standard_deviation) / (num_of_distances - 1))
-    return standard_deviation
-
-
-def test_case_mean_distance(test_case):
-    mean_distance = []
-    for i in range(len(test_case)):
-        for j in range(i + 1, len(test_case)):
-            mean_distance.append(calculate_point_distance(test_case[i], test_case[j]))
-    return sum(mean_distance) / len(mean_distance) if len(mean_distance) > 0 else 0
-
-
-def calculate_standard_deviation(points):
+def calculate_standard_deviation(data):
     dimension_data = {}
     for dimension in range(1, DIMENSIONS + 1):
-        iteration_data = []
-        for i in range(ITERATIONS):
-            test_case_data = []
-            for test_case in points[i][dimension]:
-                test_case_data.append(test_case_standard_deviation(test_case))
-            dimension_data[dimension] = test_case_data
-        iteration_data.append(dimension_data)
+        test_case_data = {}
+        for test_case in TEST_CASES:
+            iteration_data = []
+            for i in range(ITERATIONS):
+                iteration_data.append(data[i][dimension][test_case])
+            test_case_data[test_case] = statistics.stdev(iteration_data)
+        dimension_data[dimension] = test_case_data
     return dimension_data
 
 
-def calculate_mean_distance(points):
+def calculate_mean(data):
     dimension_data = {}
     for dimension in range(1, DIMENSIONS + 1):
-        iteration_data = []
-        for i in range(ITERATIONS):
-            test_case_data = []
-            for test_case in points[i][dimension]:
-                test_case_data.append(test_case_mean_distance(test_case))
-            iteration_data.append(test_case_data)
-        dimension_data[dimension] = iteration_data
+        test_case_data = {}
+        for test_case in TEST_CASES:
+            iteration_data = []
+            for i in range(ITERATIONS):
+                iteration_data.append(data[i][dimension][test_case])
+            test_case_data[test_case] = statistics.mean(iteration_data)
+        dimension_data[dimension] = test_case_data
+    return dimension_data
+
+
+def test_case_standard_deviation_distance(test_case):
+    distances = []
+    for i in range(len(test_case)):
+        for j in range(i + 1, len(test_case)):
+            distances.append(calculate_point_distance(test_case[i], test_case[j]))
+    return statistics.stdev(distances)
+
+
+def test_case_mean_distance(test_case):
+    distances = []
+    for i in range(len(test_case)):
+        for j in range(i + 1, len(test_case)):
+            distances.append(calculate_point_distance(test_case[i], test_case[j]))
+    return statistics.mean(distances)
+
+
+def calculate_standard_deviation_distance(data):
+    iteration_data = {}
+    for i in range(ITERATIONS):
+        dimension_data = {}
+        for dimension in range(1, DIMENSIONS + 1):
+            test_case_data = {}
+            for test_case in TEST_CASES:
+                test_case_data[test_case] = data[i][dimension][test_case]
+            test_case_data[test_case] = test_case_standard_deviation_distance(iteration_data)
+            dimension_data[dimension] = test_case_data
+    return dimension_data
+
+
+def calculate_mean_distance(data):
+    dimension_data = {}
+    for dimension in range(1, DIMENSIONS + 1):
+        test_case_data = {}
+        for test_case in TEST_CASES:
+            iteration_data = []
+            for i in range(ITERATIONS):
+                print(data[i][dimension][test_case])
+                iteration_data.append(data[i][dimension][test_case][0])
+            test_case_data[test_case] = test_case_mean_distance(iteration_data)
+        dimension_data[dimension] = test_case_data
     return dimension_data
 
 
@@ -85,23 +115,22 @@ def plot_data_to_file(data):
 
 
 def run():
-    dimension_data = generate_points(DIMENSIONS)
-    print(json.dumps(dimension_data, indent=2))
+    data = generate_points()
     # print(json.dumps(dimension_data, indent=2))
     # print(json.dumps(dimension_data, indent=2))
-    mean_distance = calculate_mean_distance(dimension_data)
-    standard_deviation = calculate_standard_deviation(dimension_data)
+    mean_distance = calculate_mean_distance(data)
+    standard_deviation = calculate_standard_deviation_distance(data)
 
     print("MD")
     print(json.dumps(mean_distance, indent=2))
     print("SD")
     print(json.dumps(standard_deviation, indent=2))
     # ,print(list(zip(*[mean_distance[i][1] for i in range(ITERATIONS)])))
-    print(list(map(lambda l: sum(l) / len(l), list(zip(*[mean_distance[i][1] for i in range(ITERATIONS)])))))
+    # print(list(map(lambda l: sum(l) / len(l), list(zip(*[mean_distance[i][1] for i in range(ITERATIONS)])))))
     mean_mean_distance = {dimension: list(map(lambda l: sum(l) / len(l), list(zip(*[mean_distance[i][dimension] for i in range(ITERATIONS)])))) for dimension in range(1, DIMENSIONS + 1)}
-    print(json.dumps(mean_mean_distance, indent=2))
+    # print(json.dumps(mean_mean_distance, indent=2))
     mean_standard_deviation = {dimension: list(map(lambda l: sum(l) / len(l), list(zip(*[standard_deviation[i][dimension] for i in range(ITERATIONS)])))) for dimension in range(1, DIMENSIONS + 1)}
-    print(json.dumps(mean_standard_deviation, indent=2))
+    # print(json.dumps(mean_standard_deviation, indent=2))
     # print(json.dumps([{j: mean_distance[i][j] - standard_deviation[i][j] for j in range(1, DIMENSIONS + 1)} for i in range(len(mean_distance))], indent=2))
 
     # print(json.dumps(dimension_data, indent=2))
